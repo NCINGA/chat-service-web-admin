118c6f50dcfb6863141096753cc2af0b1bdb57b3
imeshaDewmin <dewmin2001@gmail.com>
2025-01-29 16:03:44 +0530

    LoginPage

--- src/components/LoginScreenComponent/signupform.tsx
+++ src/components/LoginScreenComponent/signupform.tsx
@@ -1,6 +1,169 @@
-const SignupForm = () => {
-  return (
-    <div>signupForm</div>
-  )
-}
-export default SignupForm;
+// import React, { useState, FormEvent } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Button from "@mui/material/Button";
+// import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+// import Box from "@mui/material/Box";
+// import Typography from "@mui/material/Typography";
+// import HarvestIQ from "../../assets/bg.webp";
+// import Grid from "@mui/material/Grid";
+
+// const SignupForm: React.FC = () => {
+//   // TypeScript types for the state variables
+//   const [name, setName] = useState<string>("");
+//   const [email, setEmail] = useState<string>("");
+//   const [password, setPassword] = useState<string>("");
+//   const [confirmPassword, setConfirmPassword] = useState<string>("");
+//   const navigate = useNavigate();
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+    
+//     if (password !== confirmPassword) {
+//       alert("Passwords do not match");
+//       return;
+//     }
+    
+//     alert("Signup Successful!");
+//     navigate("/dashboard");
+//   };
+
+//   return (
+//     <Box
+//       sx={{
+//         width: "80%",
+//         maxWidth: "400px",
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         height: "100%",
+//         justifyContent: "center",
+//       }}
+//     >
+//       <img
+//         src={HarvestIQ}
+//         alt="logo"
+//         style={{
+//           width: "20vw",
+//           maxWidth: "150px",
+//           paddingBottom: "5px",
+          
+//         }}
+//       />
+//       <Typography
+//         component="h1"
+//         variant="h5"
+//         style={{ fontFamily: "Poppins, sans-serif", color: "#40BFB4" }}
+//       >
+//         Create Your Account
+//       </Typography>
+//       <Box
+//         component="form"
+//         noValidate
+//         onSubmit={handleSubmit}
+//         sx={{
+//           mt: 1,
+//           width: "100%",
+//           padding: "10px",
+//           backdropFilter: "blur(100px)",
+//         }}
+//       >
+//         <TextField
+//           margin="normal"
+//           required
+//           fullWidth
+//           id="name"
+//           label="Full Name"
+//           name="name"
+//           autoComplete="name"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//           sx={{
+//             "& label": { color: "#40BFB4" },
+//             "& .MuiOutlinedInput-root fieldset": { borderColor: "#40BFB4" },
+//             "& .MuiInputBase-root": { fontSize: "1rem" }, // Fix for magnification
+//           }}
+//         />
+//         <TextField
+//           margin="normal"
+//           required
+//           fullWidth
+//           id="email"
+//           label="Email Address"
+//           name="email"
+//           autoComplete="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           sx={{
+//             "& label": { color: "#40BFB4" },
+//             "& .MuiOutlinedInput-root fieldset": { borderColor: "#40BFB4" },
+//             "& .MuiInputBase-root": { fontSize: "1rem" }, // Fix for magnification
+//           }}
+//         />
+//         <TextField
+//           margin="normal"
+//           required
+//           fullWidth
+//           name="password"
+//           label="Password"
+//           type="password"
+//           id="password"
+//           autoComplete="new-password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           sx={{
+//             "& label": { color: "#40BFB4" },
+//             "& .MuiOutlinedInput-root fieldset": { borderColor: "#40BFB4" },
+//             "& .MuiInputBase-root": { fontSize: "1rem" }, // Fix for magnification
+//           }}
+//         />
+//         <TextField
+//           margin="normal"
+//           required
+//           fullWidth
+//           name="confirmPassword"
+//           label="Confirm Password"
+//           type="password"
+//           id="confirmPassword"
+//           autoComplete="new-password"
+//           value={confirmPassword}
+//           onChange={(e) => setConfirmPassword(e.target.value)}
+//           sx={{
+//             "& label": { color: "#40BFB4" },
+//             "& .MuiOutlinedInput-root fieldset": { borderColor: "#40BFB4" },
+//             "& .MuiInputBase-root": { fontSize: "1rem" }, // Fix for magnification
+//           }}
+//         />
+//         <FormControlLabel
+//           control={<Checkbox value="agree" sx={{ color: "#F68D3D" }} />}
+//           label="I agree to the Terms and Conditions"
+//           sx={{ "& .MuiFormControlLabel-label": { color: "#F68D3D" } }}
+//         />
+//         <Button
+//           type="submit"
+//           variant="outlined"
+//           fullWidth
+//           sx={{
+//             mt: 3,
+//             mb: 2,
+//             fontFamily: "Poppins, sans-serif",
+//             borderColor: "#ffffff",
+//             backgroundColor: "#40BFB4",
+//             color: "#ffffff",
+//             "&:hover": {
+//               backgroundColor: "#ffffff",
+//               color: "#40BFB4",
+//               borderColor: "#40BFB4",
+//             },
+//           }}
+//         >
+//           Sign Up
+//         </Button>
+//         <Grid container spacing={2}></Grid>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default SignupForm;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
-import LoginForm from '../components/LoginScreenComponent/loginForm';
-import SignupForm from '../components/LoginScreenComponent/signupform';
-const Login = () => {
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import BgImgLogin from "../assets/bg.png";
+import Grid from "@mui/material/Grid2";
+import LoginForm from "../components/LoginScreenComponent/loginForm";
+// import SignupForm from "../components/LoginScreenComponent/signupform";
+// import { Tabs, Tab } from "@mui/material";
+import { SetStateAction, useState } from "react";
+
+const defaultTheme = createTheme();
+
+export default function Login() {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  // const handleTabChange = (_event: any, newValue: SetStateAction<number>) => {
+    // setSelectedTab(newValue);
+  // };
+
   return (
-    <>
-      <LoginForm />
-      <SignupForm/>
-    </>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ minWidth: "100vw" }}>
+        <CssBaseline />
+        <Grid
+          container
+          spacing={2}
+          size={{ xs: 12, sm: 8, md: 6 }}
+          sx={{
+            backgroundImage: `url(${BgImgLogin})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: (t) => (t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900]),
+          }}
+        />
+        <Grid
+          size={{ xs: 12, sm: 8, md: 6 }}
+          component={Paper}
+          elevation={6}
+          square
+          sx={{
+            background: "#fff",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+           
+          }}
+        >
+          {/* Tabs to toggle between Login and Signup */}
+          {/* <Tabs value={selectedTab} onChange={handleTabChange} aria-label="login-signup-tabs" >
+            <Tab label="Login" />
+            <Tab label="Signup" />
+          </Tabs> */}
+          <LoginForm/>
+
+          {/* Conditional rendering based on selectedTab */}
+          {/* // {selectedTab === 0 ? <LoginForm /> : <SignupForm />} */}
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
-export default Login
